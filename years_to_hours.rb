05ab20d485a88aefd854e8313fc2c38350fd537e
Jay <95107672+Jay-zar@users.noreply.github.com>
2022-10-26 00:07:35 -0400

    Answered SQL questions and added Ruby file for Ruby assignment.

---
+++ years_to_hours.rb
@@ -0,0 +1,17 @@
+puts "Enter a number of years"
+years = gets.chomp # this returns a string
+years = years.to_i # this converts a string to an integer
+hours = years * 365 * 24
+puts "That's #{hours} hours."
+#converting minutes to minutes
+puts "Enter a number of minutes"
+minutes = gets.chomp
+minutes minutes.to_i
+minutes = minutes * 10 * 365 * 24 * 60
+puts "That's #{minutes} minutes"
+#converting a person's seconds in years to seconds
+puts "Enter your seconds in years"
+seconds = gets.chomp
+seconds = seconds.to_i
+seconds = seconds * 365 * 24 * 60 * 60
+puts "That's #{seconds} seconds. Thanks for playing"
