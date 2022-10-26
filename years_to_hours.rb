7a4ca96621ab68a5ce1387581394ead2c3dc15db
Jay <95107672+Jay-zar@users.noreply.github.com>
2022-10-26 00:14:46 -0400

    Completed assignment

--- years_to_hours.rb
+++ years_to_hours.rb
@@ -3,14 +3,14 @@ years = gets.chomp # this returns a string
 years = years.to_i # this converts a string to an integer
 hours = years * 365 * 24
 puts "That's #{hours} hours."
-#converting minutes to minutes
-puts "Enter a number of minutes"
+#converting decades to minutes
+puts "Enter a number of decades"
 minutes = gets.chomp
 minutes minutes.to_i
 minutes = minutes * 10 * 365 * 24 * 60
 puts "That's #{minutes} minutes"
-#converting a person's seconds in years to seconds
-puts "Enter your seconds in years"
+#converting a person's age in years to seconds
+puts "Enter your age in years"
 seconds = gets.chomp
 seconds = seconds.to_i
 seconds = seconds * 365 * 24 * 60 * 60
